6b1f6d6074cc4a3e4004184bee3d14801e14ae0b
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-28 06:47:10 +0900

    Author:hjyun Before use style component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App" />
+      <Fragment>
+        <Button />
+        <Button danger/>
+      </Fragment>
     );
   }
+}
+
+const Button = ({danger}) => {
+  return (
+    <button className={danger ? "button button__danger" : "button button__success"}>hello!</button>
+  )  
 }
 
 export default App;
