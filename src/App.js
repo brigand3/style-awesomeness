210ce7c03b27dcdc82e4d5f724e5cbc949d580bc
Hyunjin Yun <sayconclusion@gmail.com>
2018-12-28 18:39:52 +0900

    Author:hjyun Use animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
 createGlobalStyle`
   body{
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <Container>
         <Button>Hello!</Button>
-        <Button danger>Hello!</Button>
+        <Button danger rotationTime={10}>Hello!</Button>
         <Anchor as="a" href="http://google.com">Hello!</Anchor>
       </Container>
     );
@@ -39,10 +39,24 @@ const Button = styled.button`
     outline: none;
   }
   background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")}
+  ${props => {
+    if (props.danger) {
+      return css`animation: ${rotation} ${props.rotationTime}s linear infinite`;
+    }
+  }};
 `;
 
 const Anchor = styled(Button)`
   text-decoration:none;
 `;
+
+const rotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
 
 export default App;
